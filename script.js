df911caba87f189ecb44af8dbf38bf386fb601c9
Shubham <shubhamchachan9@gmail.com>
2022-05-01 12:49:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var seconds=document.getElementById('seconds');
+var milliseconds=document.getElementById('miliseconds');
+var start=document.getElementById('start');
+var stop=document.getElementById('stop');
+var reset=document.getElementById('reset');
+var sec=0;
+var millisec=0;
+
+
+
+const timer=()=>{
+    millisec++;  
+}
+console.log('timer', millisec)
+console.log('timer', millisec)
+console.log('timer', millisec)
+console.log('timer', millisec)
+console.log('timer', millisec)
+console.log('timer', millisec)
+//fatal: not a git repository 
+console.log('timer', millisec)
+const timer=()=>{
+    millisec++;  
+}
